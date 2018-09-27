14735bd5898ac98d512f27e7807c2ce137bb16e1
Bullock <matthew.bullock@M11807.local>
2018-09-27 11:51:16 +0100

    init Heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,24 @@ import { NavLink } from 'react-router-dom'
 
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+ componentDidMount() {
+   this.callApi()
+     .then(res => this.setState({ response: res.express }))
+     .catch(err => console.log(err));
+ }
+
+ callApi = async () => {
+   const response = await fetch('/api/hello');
+   const body = await response.json();
+
+   if (response.status !== 200) throw Error(body.message);
+
+   return body;
+  };
 
   goTo(route) {
     this.props.history.replace(`/${route}`)
@@ -35,6 +53,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <h2>{this.state.response}</h2>
         <NewReleases />
       </div>
     );
